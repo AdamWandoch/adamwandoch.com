d714d6468625211de207806e5c7e58f56f028167
Adam Wandoch <wandoch.adam@gmail.com>
2022-05-12 01:21:28 +0100

    wrote a simple script modifing dom elements

--- script/index.js
+++ script/index.js
@@ -1,3 +1,26 @@
 window.addEventListener("DOMContentLoaded", function() {
-  
-});
+    const words = [
+      "create",
+      "software",
+      "Help",
+    ]
+
+    for (let i = 0; i < words.length; i++) {
+      const element = words[i];
+      applyAccent(element);
+    }
+
+  }) 
+
+function applyAccent(searchString) {
+  const paragraphs = document.getElementsByTagName("p");
+  for (let i = 0; i < paragraphs.length; i++) {
+    let paragraph = paragraphs[i];
+    if (paragraph.innerHTML.includes(searchString)) {
+      let newString = paragraph.innerHTML.replace(
+                      searchString,
+                      "<span class=\"accent\">" + searchString + "</span>");
+      paragraph.innerHTML = newString;
+    }
+  }
+}
