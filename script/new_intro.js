17d567b37fdecb9b748ead79771111ef1b9c11d6
Adam Wandoch <wandoch.adam@gmail.com>
2022-05-18 03:06:10 +0100

    animations redone

--- script/new_intro.js
+++ script/new_intro.js
@@ -1,9 +1,65 @@
+"use strict";
+
+
+
 window.addEventListener("DOMContentLoaded", function() {
+  setTimeout(() => {
+    typingAnimation("p1", "Hi, I'm Adam.", 100);
+  }, 1000);
+
+  setTimeout(() => {
+    typingAnimation("p2", "I'm looking for a change.", 100);
+  }, 3500);
+
+  setTimeout(() => {
+    typingAnimation("header1", "Are you looking", 100);
+  }, 7500);
+  
+  setTimeout(() => {
+    typingAnimation("header2", "for a developer?", 100);
+    
+  }, 9500);
+
+  setTimeout(() => {
+    document.getElementById("portrait").classList.add("revealed");
+  }, 11500);
+  
+  setTimeout(() => {
+    document.getElementById("cta").classList.add("revealed");
+  }, 11800);
+  
+  setTimeout(() => {
+    document.getElementById("desktop").classList.add("revealed");
+  }, 12100);
+
+  setTimeout(() => {
+    let h2 = document.getElementById("header2");
+    h2.innerHTML = h2.innerHTML
+                     .replace("developer", 
+                              "<span id='developer'>developer</span>");
+  }, 13000);
+
   setTimeout(() => {
     developerWordAnimation();
     desktopAnimation();
-  }, 3000);
+  }, 14000);
 });
+
+
+function typingAnimation(id, text, speed) {
+  let element = document.getElementById(id);
+  let textPosition = 0;
+  element.textContent = "";
+  
+  writingFunc();
+  
+  function writingFunc() {
+    element.textContent = text.substring(0, textPosition);
+    if(textPosition++ !== text.length) {
+      setTimeout(writingFunc, speed);
+    }
+  }
+}
 
 function developerWordAnimation() {
   const developer = document.getElementById("developer");
@@ -19,5 +75,3 @@ function desktopAnimation() {
     desktop.classList.toggle("desktop-change");
   }, 1200);
 }
-
-
